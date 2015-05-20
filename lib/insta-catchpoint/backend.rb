3f70a6e19029867edb574a8fac683cd91375bb5a
D. Pramann <daniel@pramann.org>
2015-05-20 03:29:09 -0500

    Working on human output, giving pull an $id works now

--- lib/insta-catchpoint/backend.rb
+++ lib/insta-catchpoint/backend.rb
@@ -10,10 +10,24 @@ module InstaCatchpoint
       @catchpoint = Api.new(conf)
     end
     
-    # Search method, look for website
-    def contains(website)
-      test = pull(website)
-      puts test
+    # Search method, look for website in test_url, by id, etc.
+    def contains(string, style)
+      if !/\A\d+\z/.match(string)
+        # Not a positive number, url?
+      else
+        # Number
+        test = pull_id(string)
+      end
+      case style
+        when 'human'
+            print_human(test)
+        when 'json'
+            print_json(test)
+        when 'hash'
+            print_hash(test)
+        else
+          puts "You're not supposed to be here. Didn't Runner catch the unregcongized style?"
+      end
     end
     
     # Create a test for Catchpoint
@@ -25,29 +39,19 @@ module InstaCatchpoint
     def dump(style)
       case style
         when 'human'
-          pullall.each do |test|
-            puts "Name: " + test.name
-            puts "Status: " + test.status.name
-            puts "Id: " + test.id.to_s
-            puts "Type: " + test.type.name
-            unless ( defined?(test.frequency.name) ).nil?
-              puts "Frequency: " + test.frequency.name
-            end
-            puts "URL(s): " + test.test_url
-            puts
+          pull_all.each do |test|
+            print_human(test)
           end
         when 'json'
-          pullall.each do |test|
-            puts test.to_json
-            puts
+          pull_all.each do |test|
+            print_json(test)
           end
         when 'hash'
-          pullall.each do |test|
-            puts test.to_h
-            puts
+          pull_all.each do |test|
+            print_hash(test)
           end
         else
-          puts "You're not supposed to be here. Didn't Runner catch the unregconized style?"
+          puts "You're not supposed to be here. Didn't Runner catch the unregcongized style?"
       end
     end
     
@@ -55,7 +59,7 @@ module InstaCatchpoint
     private
     
       # Method to return all tests in an array
-      def pullall
+      def pull_all
         storage = Array.new
         @catchpoint.get("tests")["items"].each do |test|
           test = Dish(test)
@@ -71,12 +75,70 @@ module InstaCatchpoint
       
       # Method to look for an id of a test in Catchpoint
       def pull_id(id)
-        return Dish(@catchpoint.get("tests?id=#{id}"))
+        return Dish(@catchpoint.get("tests/#{id}"))
       end
       
       # Method to push something up to Catchpoint
-      def push
+      def push_test
         #@catchpoint.put(x)
+      end
+      
+      def print_human(test)
+        puts "Name: " + test.name
+        unless ( defined?(test.description) ).nil?
+          puts "Desc: " + test.description
+        end
+        puts "Status: " + test.status.name
+        puts "Id: " + test.id.to_s
+        unless ( defined?(test.type.name) ).nil?
+          puts "Type: " + test.type.name
+        end
+        unless ( defined?(test.test_type.name) ).nil?
+          puts "Type: " + test.test_type.name
+        end
+        unless ( defined?(test.monitors['name']) ).nil?
+          string = "Monitor(s): "
+          test.monitors.each do |monitor|
+            string += monitor.name + ' '
+          end
+          puts string              
+        end
+        unless ( defined?(test.monitor.name) ).nil?
+          puts "Monitor: " + test.monitor.name
+        end
+        unless ( defined?(test.http_method.name) ).nil?
+          puts "HTTP: " + test.http_method.name.upcase + " request"
+        end
+        unless ( defined?(test.frequency.name) ).nil?
+           puts "Frequency: " + test.frequency.name
+        end
+        unless ( defined?(test.advanced_settings) ).nil?
+          puts "Advanced Settings:"
+          test.advanced_settings.to_h.each do |key, value|
+            puts "  " + key.capitalize + ": "
+            if value.is_a?(Hash)
+              value.each do |key, value|
+                if value.is_a?(Hash)
+                  puts "    " + key.capitalize + ' = ' + value['name']
+                else
+                  puts "    " + key.capitalize + ' = ' + value.to_s
+                end
+              end
+            else
+              puts value
+            end
+          end
+        end
+        puts "URL(s): " + test.test_url
+        puts
+      end
+      
+      def print_json(test)
+        puts test.to_json
+      end
+      
+      def print_hash(test)
+        puts test.to_h
       end
   end
 end
--- lib/insta-catchpoint/runner.rb
+++ lib/insta-catchpoint/runner.rb
@@ -4,33 +4,51 @@ require 'thor'
 module InstaCatchpoint
   class Runner < Thor    
     class_option :verbose, :type => :boolean
-    class_option :force, :type => :boolean
     
-    desc "pull WEBSITE", "Pull down any tests for WEBSITE"
-    def pull(website)
-        this = Backend.new
-        log("API - Get tests for #{website}")
-        #this.contains(website)
+    desc "pull IDENTIFIER STYLE", "Pull test that matches/contains/is; output in json, hash, human"
+    def pull(identifier, style)
+      identifier.chomp
+      style.chomp
+      this = Backend.new
+      log("Requesting test(s) that matches/contains/is: #{identifier}")
+      case style
+        when 'human'
+          log("Requesting test(s) in a human readable format\n\n")
+          this.contains(identifier, style)
+        when 'json'
+          log("Requesting test(s) in json format\n\n")
+          this.contains(identifier, style)
+        when 'hash'
+          log("Requesting test(s) in hash format\n\n")
+          this.contains(identifier, style)
+        else
+          log("Requesting all tests in a data format of #{style}\n\n")
+          puts "I don't recongize that style of data..."
+      end
     end
+    
     desc "pullall STYLE", "Pull down ALL tests in Catchpoint; output in json, hash, human"
     def pullall(style)
       style.chomp
       this = Backend.new
-      puts "WARNING: This may take a while!"
-      puts
+      log("WARNING: This may take a while!")
       case style
         when 'human'
+          log("Requesting all tests in a human readable format\n\n")
           this.dump(style)
         when 'json'
+          log("Requesting all tests in json format\n\n")
           this.dump(style)
         when 'hash'
+          log("Requesting all test in hash format\n\n")
           this.dump(style)
         else
-          log("Requesting all tests in a data format of #{style}")
-          puts "I don't reconigize that style of data..."
+          log("Requesting all tests in a data format of #{style}\n\n")
+          puts "I don't recongize that style of data..."
       end
     end
-    desc "add WEBSITE", "Add default tests for WEBSITE"
+    
+    desc "add WEBSITE", "Add defined default tests for WEBSITE"
     def add(website)
         this = Backend.new
         log("API - Add tests for #{website}")
